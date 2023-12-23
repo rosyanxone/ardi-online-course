bb01b3f9925e60622a47487a3b141967759a6415
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2023-12-23 10:38:41 +0800

    home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,99 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container py-4">
+    <div class="">
+      <div class="d-flex justify-content-center flex-wrap gap-3">
+
+        <div class="card w-card">
+          <div class="card-body justify-content-between d-flex flex-column gap-2">
+            <span>
+              <h5 class="card-title fw-bold">Card title</h5>
+              <p class="card-text d-none d-sm-block d-lg-none">
+                {{ Str::limit("Some quick example text to build on the card title and make up the bulk of the card's content.", 60) }}
+              </p>
+              <p class="card-text d-block d-sm-none d-lg-block">
+                {{ Str::limit("Some quick example text to build on the card title and make up the bulk of the card's content.", 95) }}
+              </p>
+            </span>
+            <a class="btn subtle-btn-secondary fw-medium text-uppercase text-white" href="#">Detail Kursus</a>
+          </div>
+        </div>
+        <div class="card w-card">
+          <div class="card-body justify-content-between d-flex flex-column gap-2">
+            <span>
+              <h5 class="card-title fw-bold">Card title</h5>
+              <p class="card-text d-none d-sm-block d-lg-none">
+                {{ Str::limit("Some quick example text to build on the card title and make up the bulk of the card's content.", 60) }}
+              </p>
+              <p class="card-text d-block d-sm-none d-lg-block">
+                {{ Str::limit("Some quick example text to build on the card title and make up the bulk of the card's content.", 95) }}
+              </p>
+            </span>
+            <a class="btn subtle-btn-secondary fw-medium text-uppercase text-white" href="#">Detail Kursus</a>
+          </div>
+        </div>
+        <div class="card w-card">
+          <div class="card-body justify-content-between d-flex flex-column gap-2">
+            <span>
+              <h5 class="card-title fw-bold">Card title</h5>
+              <p class="card-text d-none d-sm-block d-lg-none">
+                {{ Str::limit("Some quick example text to build on the card title and make up the bulk of the card's content.", 60) }}
+              </p>
+              <p class="card-text d-block d-sm-none d-lg-block">
+                {{ Str::limit("Some quick example text to build on the card title and make up the bulk of the card's content.", 95) }}
+              </p>
+            </span>
+            <a class="btn subtle-btn-secondary fw-medium text-uppercase text-white" href="#">Detail Kursus</a>
+          </div>
+        </div>
+        <div class="card w-card">
+          <div class="card-body justify-content-between d-flex flex-column gap-2">
+            <span>
+              <h5 class="card-title fw-bold">Card title</h5>
+              <p class="card-text d-none d-sm-block d-lg-none">
+                {{ Str::limit("Some quick example text to build on the card title and make up the bulk of the card's content.", 60) }}
+              </p>
+              <p class="card-text d-block d-sm-none d-lg-block">
+                {{ Str::limit("Some quick example text to build on the card title and make up the bulk of the card's content.", 95) }}
+              </p>
+            </span>
+            <a class="btn subtle-btn-secondary fw-medium text-uppercase text-white" href="#">Detail Kursus</a>
+          </div>
+        </div>
+        <div class="card w-card">
+          <div class="card-body justify-content-between d-flex flex-column gap-2">
+            <span>
+              <h5 class="card-title fw-bold">Card title</h5>
+              <p class="card-text d-none d-sm-block d-lg-none">
+                {{ Str::limit("Some quick example text to build on the card title and make up the bulk of the card's content.", 60) }}
+              </p>
+              <p class="card-text d-block d-sm-none d-lg-block">
+                {{ Str::limit("Some quick example text to build on the card title and make up the bulk of the card's content.", 95) }}
+              </p>
+            </span>
+            <a class="btn subtle-btn-secondary fw-medium text-uppercase text-white" href="#">Detail Kursus</a>
+          </div>
+        </div>
+        <div class="card w-card">
+          <div class="card-body justify-content-between d-flex flex-column gap-2">
+            <span>
+              <h5 class="card-title fw-bold">Card title</h5>
+              <p class="card-text d-none d-sm-block d-lg-none">
+                {{ Str::limit("Some quick example text to build on the card title and make up the bulk of the card's content.", 60) }}
+              </p>
+              <p class="card-text d-block d-sm-none d-lg-block">
+                {{ Str::limit("Some quick example text to build on the card title and make up the bulk of the card's content.", 95) }}
+              </p>
+            </span>
+            <a class="btn subtle-btn-secondary fw-medium text-uppercase text-white" href="#">Detail Kursus</a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+@endsection
+
+@section('scripts')
+@endsection
